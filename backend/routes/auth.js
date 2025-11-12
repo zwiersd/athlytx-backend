754268900b41a4c31567932c6bf788eae89d5824
zwiersd <darren@zwiers.co.uk>
2025-11-12 14:04:22 +0000

    Fix auth route - comment out missing email utility

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const crypto = require('crypto');
 const { User, MagicLink, CoachAthlete } = require('../models');
-const { sendMagicLink } = require('../utils/email');
+// const { sendMagicLink } = require('../utils/email'); // TODO: Implement email service
 const { Op } = require('sequelize');
 
 /**
